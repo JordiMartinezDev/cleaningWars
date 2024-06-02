dc6f91d9c9348c480d46ec63ce7274ea7ac24772
JordiMartinezDev <jordiupc87@gmail.com>
2024-06-02 22:32:37 +0200

    TabBarIcons added

--- components/LoggedInAppNavigator.js
+++ components/LoggedInAppNavigator.js
@@ -7,6 +7,10 @@ import { useSelector } from "react-redux";
 import Week from "../screens/Week";
 import Tasks from "../screens/Tasks";
 import Configuration from "../screens/Configuration";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,11 +18,52 @@ const LoggedInAppNavigator = () => {
   const userId = useSelector((state) => state.auth.userId);
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
-      <Tab.Screen name="Week" component={Week} />
-      <Tab.Screen name="Tasks" component={Tasks} />
-      <Tab.Screen name="Configuration" component={Configuration} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Week"
+        component={Week}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="calendar-week"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Tasks"
+        component={Tasks}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="form" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Configuration"
+        component={Configuration}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
