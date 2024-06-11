9d0bf8844211d6010e7d33973faf86ad10c7b08d
JordiMartinezDev <jordiupc87@gmail.com>
2024-06-11 17:22:14 +0200

    Added Agenda component

--- screens/Week.js
+++ screens/Week.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { Agenda } from "react-native-calendars";
+import tasks from "../data/tasks.json";
 
 const Week = () => {
   return (
-    <View>
-      <Text>Week</Text>
+    <View style={style.container}>
+      <Agenda items={tasks} />{" "}
+      {/* Agenda expects the items in a different way, not inside an array but with key value pairs where key is  date like "2022-11-25" */}
     </View>
   );
 };
 
 export default Week;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
