0e7f1232423c9e166f0c15ed2fda57d9f8311501
JordiMartinezDev <jordiupc87@gmail.com>
2024-06-14 16:15:44 +0200

    Rendering mock events on week calendar

--- screens/Week.js
+++ screens/Week.js
@@ -1,12 +1,16 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Agenda } from "react-native-calendars";
-import tasks from "../data/tasks.json";
+import events from "../data/events.json";
 
 const Week = () => {
+  const renderCalendarEvent = (event) => {
+    return <Text>{event.name}</Text>;
+  };
+
   return (
     <View style={style.container}>
-      <Agenda items={tasks} />{" "}
+      <Agenda items={events} renderItem={renderCalendarEvent} />
       {/* Agenda expects the items in a different way, not inside an array but with key value pairs where key is  date like "2022-11-25" */}
     </View>
   );
