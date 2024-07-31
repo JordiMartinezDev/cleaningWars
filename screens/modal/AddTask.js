5ea4e0233212ed959492b88facb45877a2b44211
JordiMartinezDev <jordiupc87@gmail.com>
2024-07-31 08:39:38 +0200

    Styling addTask modal field cards

--- screens/modal/AddTask.js
+++ screens/modal/AddTask.js
@@ -1,10 +1,18 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Platform } from "react-native";
 import React from "react";
 
 const AddTask = ({ navigation }) => {
   return (
     <View style={styles.modalContainer}>
-      <Text></Text>
+      <Text style={styles.gridItem}>Task name/type</Text>
+      {/*this should be a modal with all tasks + add new task */}
+      <View style={styles.gridItem}>
+        <View style={styles.innerContainer}>
+          <Text>User, default is logged in</Text>
+        </View>
+      </View>
+
+      <Text style={styles.gridItem}>Day, default is selectedDay</Text>
       <Text style={{ fontSize: 30 }}>This is a modal!!</Text>
       <Button onPress={() => navigation.goBack()} title="Dismisss" />
     </View>
@@ -16,6 +24,26 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  gridItem: {
+    margin: 16,
+    height: 64,
+    borderRadius: 8,
+    elevation: 4,
+    //iOS styling shadows
+    backgroundColor: "white", // This is not iOS specific, but needed in order for iOS shadows to work
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    //no Android specific
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
   },
   formField: {
     backgroundColor: "white",
